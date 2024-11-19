c6ff55b66a955aeb1f39c084cf84a2a08cfd737f
WasanSanngern <wasan1234san@gmail.com>
2024-11-19 15:43:40 +0700

    Update knn.py

--- knn.py
+++ knn.py
@@ -3,8 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import plotly.express as px
-import matplotlib.pyplot as plt
+
 
 st.header('Wasan')
 #st.image("./pic/kairung.jpg")
