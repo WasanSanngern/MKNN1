2155be8dec89980cd3bb825981e743d0d9650899
WasanSanngern <wasan1234san@gmail.com>
2024-11-19 14:42:46 +0700

    .

--- home.py
+++ home.py
@@ -5,3 +5,10 @@ st.title("Website Developing using Python")
 st.header("Website Developing using Python")
 
 st.subheader("Wasan Sanngern")
+
+st.image('./img/IMG_0689.jpg')
+st.subheader("Wasan Sanngern")
+
+dt=pd.read_csv('./data/iris-3.csv')
+st.header()
+st.write(dt.head(10))
